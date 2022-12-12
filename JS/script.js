89467bf1d0d2cad2ec0124887d97eb57143db109
Lucas Henrique <lucas.henrique.andrade.ti@gmail.com>
2022-12-12 12:03:06 -0300

    estudos faculdade

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,3 @@
-//let num1 = prompt('insira um numero'), num2 = prompt('insira outro numero')
+let num1 = prompt('insira um numero'), num2 = prompt('insira outro numero')
 
-//let resultado = alert(num1 / num2)
-
-document.getElementById(n1)
+let resultado = alert(num1 / num2)
